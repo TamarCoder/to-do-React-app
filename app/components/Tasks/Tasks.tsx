3bd6d5fe82d164c8b0b6be1aaf420f57f91ccb49
Tamar Khuskivadze <thamuna.khuskivadze@gmail.com>
2025-06-12 19:31:48 +0400

    add change rendom back

--- app/components/Tasks/Tasks.tsx
+++ app/components/Tasks/Tasks.tsx
@@ -1,24 +1,36 @@
-import { ArrowRightOutlined, CalendarOutlined } from '@ant-design/icons';
-import styles from './Tasks.module.css';
-
+"use client";
+import { ArrowRightOutlined, CalendarOutlined } from "@ant-design/icons";
+import styles from "./Tasks.module.css";
+import { useEffect, useState } from "react";
 
 type TasksProps = {
   taskText: string;
 };
 
-export default function Tasks ({ taskText }: TasksProps) {
-    return (
-        <div className={styles.task}>
-            <div className={styles.taskWeek}>
-                <div className={styles.days}>
-                    <CalendarOutlined />
-                    <p>Monday</p>
-                </div>
-                <ArrowRightOutlined />
-            </div>
-            <div className={styles.taskText}>
-                 <p>{taskText}</p>
-            </div>
+export default function Tasks({ taskText }: TasksProps) {
+  const [bgColor, setBgColor] = useState("");
+
+  useEffect(() => {
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    
+    setBgColor(randomColor);
+  }, []);
+
+  return (
+    <div
+      className={styles.task}
+      style={{ backgroundColor: bgColor }}
+    >
+      <div className={styles.taskWeek}>
+        <div className={styles.days}>
+          <CalendarOutlined />
+          <p>Monday</p>
         </div>
-    )
+        <ArrowRightOutlined />
+      </div>
+      <div className={styles.taskText}>
+        <p>{taskText}</p>
+      </div>
+    </div>
+  );
 }
